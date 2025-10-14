831fe8201b03eacba5496a8b9fe381aeb397d1e2
0xmuka <hemasallam03@gmail.com>
2025-10-14 12:12:19 +0300

    Quartz sync: Oct 14, 2025, 12:12 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "The GOAT 𓃵",
+    pageTitle: "GOAT 𓃵",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
