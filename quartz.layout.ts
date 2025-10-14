ba9627cde06a3f336962fbf5cf3aaa9afc3ad46c
0xmuka <hemasallam03@gmail.com>
2025-10-14 11:49:43 +0300

    Quartz sync: Oct 14, 2025, 11:49 AM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -40,7 +40,7 @@ export const defaultContentPageLayout: PageLayout = {
         { Component: Component.ReaderMode() },
       ],
     }),
-    //Component.Explorer(),
+    Component.Explorer(),
   ],
   right: [
     Component.Graph(),
