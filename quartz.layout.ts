e53796e3cb01fc56249a8ff6d7c0fb9548244334
0xmuka <hemasallam03@gmail.com>
2025-10-14 11:05:12 +0300

    Quartz sync: Oct 14, 2025, 11:05 AM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -8,8 +8,9 @@ export const sharedPageComponents: SharedLayout = {
   afterBody: [],
   footer: Component.Footer({
     links: {
-      GitHub: "https://github.com/jackyzha0/quartz",
-      "Discord Community": "https://discord.gg/cRFFHYye7t",
+      GitHub: "https://github.com/0xmuka",
+      "Linkedin": "https://linkedin.com/in/0xmuka",
+      "Discord Community": "https://discord.gg/5rb9UkX4MM",
     },
   }),
 }
